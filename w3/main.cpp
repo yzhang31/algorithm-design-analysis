b36967e3cd2d7e190f8ca4cbb4965220f1f3ef2f
ZhangYanhua <yanhua.dev@gmail.com>
2015-01-13 23:59:44 +0800

    create Xcode project.

---
+++ w3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  w3
+//
+//  Created by ZhangYanhua on 1/13/15.
+//  Copyright (c) 2015 ZhangYanhua. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
